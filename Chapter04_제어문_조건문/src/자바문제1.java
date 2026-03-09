8fad3ad1707acfc8d35543bf530e4028b9dafaaa
green <green@WIN-VDC25JTH7OH>
2026-03-10 08:55:26 +0900

    ooo

---
+++ Chapter04_제어문_조건문/src/자바문제1.java
@@ -0,0 +1,106 @@
+
+public class 자바문제1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int apple = 3;
+		
+		int pencil = 5;
+		int e = 2;
+		
+		int student = 30;
+		
+		int milk = 2500;
+		
+		double height = 175.5;
+		
+		String name = "민수";
+		
+		int bus = 12;
+		int subway = 20;
+		
+		int kor = 85;
+		int eng = 90;
+		
+		double pi = 3.14;
+		
+		int notebook = 1;
+		int mouse = 2;
+		int keyboard = 1;
+		
+		int sum = 5 + 3;
+		System.out.println(sum);
+		int minus = 10-4;
+		System.out.println(minus);
+		int gob = 6*7;
+		System.out.println(gob);
+		double nanugi = 20/4.0;
+		System.out.println(nanugi);
+		double namuji = 15%4;
+		System.out.println(namuji);
+		
+		int a = (8+2)*3;
+		System.out.println(a);
+		
+		int a1 = 10;
+		a1++;
+		System.out.println(a1);
+		
+		int a2 = 10;
+		a2--;
+		System.out.println(a2);
+		
+		int a3 = 7;
+		int b3 = 5;
+		boolean result = a3>b3;
+		System.out.println(result);
+		
+		int aa = 5;
+		int bb = 10;
+		if(aa>3 && bb<20) {
+			System.out.println("참");
+		}else {
+			System.out.println("거짓");
+		}
+		
+		int num = 11;
+		if(num>10) {
+			System.out.println(num+"이 10보다 큽니다");
+		}
+		
+		int score = 60;
+		if(score>=60) {
+			System.out.println("합격");
+		}
+		
+		int age = 20;
+		if(age>=20) {
+			System.out.println("성인입니다.");
+		}
+		
+		int num1 = 20;
+		if(num1%2==0) {
+			System.out.println("짝수입니다.");
+		}else {
+			System.out.println("홀수입니다.");
+		}
+		
+		int num2 = 60;
+		if(num2>=60) {
+			System.out.println("합격입니다.");
+		}else {
+			System.out.println("불합격입니다.");
+		}
+		
+		
+		int temperature = 30;
+		if(temperature>=30) {
+			System.out.println("덥다");
+		}else {
+			System.out.println("덥지 않다");
+		}
+		
+		
+	}
+
+}
