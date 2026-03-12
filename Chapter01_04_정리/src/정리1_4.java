570b2a3670a8207aa1356603fca1f41136d07410
green <green@WIN-VDC25JTH7OH>
2026-03-12 11:07:08 +0900

    ooo

---
+++ Chapter01_04_정리/src/정리1_4.java
@@ -0,0 +1,38 @@
+/*
+ * 프로그램 순서
+ * 데이터 저장 ======= 데이터 가공 ====== 화면 출력
+ * 변수 : 데이터 한개 저장
+ * 배열 : 같은 데이터 여러개 저장
+ * 		 ------------------
+ * 		 1차원배열
+ * 클래스 : 다른 데이터 여러개 저장
+ * 		 => 사용자 정의 데이터
+ * 
+ * 변수 : 한개의 데이터 저장
+ * 		 --------------
+ * 		 메모리 크기 결정
+ * 		 ----- 기본형(데이터형, 자료형)
+ * ***
+ * 	 정수 byte / int long
+ * 	 실수 double
+ * 	 논리 boolean
+ * 	 문자 char
+ * 	 문자열 String
+ *
+ */   
+import java.awt.*;
+import java.awt.event.KeyListener;
+
+import javax.swing.*;
+public class 정리1_4{
+	GameView gv=new GameView();
+	public 정리1_4()
+	{
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new 정리1_4();
+	}
+
+}
