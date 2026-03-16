7e6321028e1cd7918e068ebcdfb094b0d83493fc
green <green@WIN-VDC25JTH7OH>
2026-03-16 16:04:54 +0900

    ooo

---
+++ Chapter05_데이터관리(배열)_1/src/데이터정렬_선택정렬.java
@@ -0,0 +1,43 @@
+/*
+ * 
+ */
+import java.util.Arrays;
+public class 데이터정렬_선택정렬 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr=new int[5];
+		// 초기화
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=(int)(Math.random()*100)+1; // 1~100
+		}
+		
+		System.out.print("정렬 전:");
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println("\n정렬 후:");
+		
+		for(int i=0;i<arr.length-1;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				// DESC => 내림차순 은 아래 > 를 < 로 바꿔주기만 하면 된다.
+				if(arr[i]>arr[j])
+				{
+					int temp=arr[i]; // 값 교환 => 임시변수 설정
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+			System.out.println(Arrays.toString(arr));
+		}
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+	}
+
+}
