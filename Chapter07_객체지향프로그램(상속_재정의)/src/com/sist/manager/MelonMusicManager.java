36ed3a89c3fe528a62e1f3e99d0cf052f307fb2a
green <green@WIN-VDC25JTH7OH>
2026-03-25 09:02:19 +0900

    ooo

---
+++ Chapter07_객체지향프로그램(상속_재정의)/src/com/sist/manager/MelonMusicManager.java
@@ -0,0 +1,12 @@
+package com.sist.manager;
+
+public class MelonMusicManager extends GenieMusicManager{
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+
+}
