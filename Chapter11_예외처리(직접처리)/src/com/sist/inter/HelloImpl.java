208600b59c5a7a374864ba626245a6df9b97d3d6
green <green@BOOK-GCETD233AQ>
2026-04-07 17:47:34 +0900

    ooo

---
+++ Chapter11_예외처리(직접처리)/src/com/sist/inter/HelloImpl.java
@@ -0,0 +1,10 @@
+package com.sist.inter;
+
+public class HelloImpl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Chapter11_예외처리(직접처리)/src/예외처리_5.java
@@ -0,0 +1,20 @@
+/*
+ * 1. 인터페이스
+ * 	=> 표준화 가능
+ * 	=> 소스의 통일화
+ * 	=> 다른 클래스와 연결시에 주로 사용 : 리모콘
+ * 	   -------------------------
+ * 		=> 결합성이 낮아진다
+ * 		=> client == server
+ * 
+ * 
+ * 
+ */
+public class 예외처리_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
